6e2c9aca7ee3656a257250ee67d22d4786fc0ae4
sadelk <91189378+sadelk@users.noreply.github.com>
2021-09-23 14:22:16 +0530

    Add files via upload

--- subnet.tf
+++ subnet.tf
@@ -1,11 +1,11 @@
-resource "azurerm_subnet" "sadelkSubNet1" {
+resource "azurerm_subnet" "sadeSubNet1" {
   name                 = var.subnetNm1
   resource_group_name  = azurerm_resource_group.sadelk-grp.name
   virtual_network_name = azurerm_virtual_network.sadelkVirtualNetwork.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_subnet" "sadelkSubNet2" {
+resource "azurerm_subnet" "sadeSubNet2" {
   name                 = var.subnetNm2
   resource_group_name  = azurerm_resource_group.sadelk-grp.name
   virtual_network_name = azurerm_virtual_network.sadelkVirtualNetwork.name
